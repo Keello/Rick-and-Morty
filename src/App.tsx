50da70f9dfa05fcbac3a58e287915d113356c610
Oleg <Keello27@yandex.ru>
2025-10-29 17:19:34 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import styles from './App.module.scss';
+import Card from '@components/card/Card';
+
+function App() {
+  return (
+    <section className={styles.wrapper}>
+      <Card />
+    </section>
+  );
+}
+
+export default App;
